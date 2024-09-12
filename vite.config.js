fa9e80fbb958f44cf90b5f327708bc2ab8b558dd
Lakshan-Madushanka <epmadushanka@gmail.com>
2024-09-12 19:29:54 +0530

    WIP

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            hotFile: 'public/reviews.hot', // Most important lines
+            hotFile: 'public/package.hot', // Most important lines
             buildDirectory: '/build', // Most important lines
             input: ['resources/js/app.js'],
             refresh: true,
